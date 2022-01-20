5cacb80b5460e0803b167bd1e4f81172f7f4a3a2
Akshit Kumar <akshitkumar450@gmail.com>
2022-01-20 18:17:38 +0530

    header btns

---
+++ components/HeaderTab.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+
+const HeaderTab = () => {
+  const [select, setSelect] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Buttons
+        title="delivery"
+        viewStyles={{
+          backgroundColor: select ? "black" : "white",
+        }}
+        textStyles={{
+          color: select ? "white" : "black",
+        }}
+        select={select}
+        setSelect={setSelect}
+      />
+
+      <Buttons
+        title="pickup"
+        viewStyles={{
+          backgroundColor: select ? "white" : "black",
+        }}
+        textStyles={{
+          color: select ? "black" : "white",
+        }}
+        select={select}
+        setSelect={setSelect}
+      />
+    </View>
+  );
+};
+
+const Buttons = ({ title, select, viewStyles, textStyles, setSelect }) => {
+  return (
+    <TouchableOpacity onPress={() => setSelect(!select)}>
+      <View
+        style={{
+          paddingVertical: 6,
+          paddingHorizontal: 16,
+          borderRadius: 100,
+          ...viewStyles,
+        }}>
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: 800,
+            ...textStyles,
+          }}>
+          {title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default HeaderTab;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import HeaderTab from "../components/HeaderTab";
+
+const Home = () => {
+  return (
+    <View>
+      <HeaderTab />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
